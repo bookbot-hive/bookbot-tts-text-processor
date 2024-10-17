cddd391dac6c8a4ec63982ee8de7c538f7946600
David Samuel <davidsamuel.7878@gmail.com>
2024-10-17 18:12:59 +1100

    Clean code

--- phoneme_emphasis/__init__.py
+++ phoneme_emphasis/__init__.py
@@ -3,8 +3,3 @@ from .gruut_symbols import phonemes_to_ids, ids_to_phonemes
 from .utils import IPA_LIST, UNICODE_NORM_FORM
 
 __all__ = ['EmphasisModel', 'phonemes_to_ids', 'ids_to_phonemes', 'IPA_LIST', 'UNICODE_NORM_FORM']
-
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
