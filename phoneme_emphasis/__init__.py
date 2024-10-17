4758bdc17e7148893dbac38c2628763b48a5b957
David Samuel <davidsamuel.7878@gmail.com>
2024-10-17 20:00:13 +1100

    Refactor code

--- phoneme_emphasis/__init__.py
+++ phoneme_emphasis/__init__.py
@@ -1,6 +1,5 @@
 from .emphasis_model import EmphasisModel
 from .gruut_symbols import phonemes_to_ids, ids_to_phonemes
 from .utils import IPA_LIST, UNICODE_NORM_FORM
-from ._version import __version__
 
 __all__ = ['EmphasisModel', 'phonemes_to_ids', 'ids_to_phonemes', 'IPA_LIST', 'UNICODE_NORM_FORM']
