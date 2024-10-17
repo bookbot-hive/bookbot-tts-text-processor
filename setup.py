87b27b713e10b1130fb6785502fdf2d9c93c4e7b
David Samuel <davidsamuel.7878@gmail.com>
2024-10-17 18:02:48 +1100

    File restructure

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="phoneme_emphasis",
     version="0.1.0",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
     install_requires=[
         "torch",
         "gruut",
