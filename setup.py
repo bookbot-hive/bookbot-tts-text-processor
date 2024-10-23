670d34e3de8858479217bf187949653928b067c2
David Samuel <davidsamuel.7878@gmail.com>
2024-10-23 19:10:41 +1100

    Update dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
+import os
 
-# Read the contents of requirements.txt
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+def read_requirements():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    try:
+        with open(os.path.join(current_dir, 'requirements.txt'), 'r') as f:
+            return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+    except FileNotFoundError:
+        return []
 
 setup(
     name="text_processor",
     packages=find_packages(),
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    install_requires=requirements,
+    nstall_requires=read_requirements(),
     author="David Samuel Setiawan",
     author_email="davidsamuel.7878@gmail.com",
     description="A package processing input text for Bookbot Optispeech TTS",
