f9d9b0db3cf3e8758b52fd7f27be7133268e6a96
David Samuel <davidsamuel.7878@gmail.com>
2024-11-15 21:36:08 +1100

    Add emphasis lookup lexicon

---
+++ snapshot_inference.py
@@ -0,0 +1,29 @@
+import os
+from text_processor import TextProcessor
+
+
+model_dirs = {
+"en": "bookbot/roberta-base-emphasis-onnx-quantized",
+"sw": "",
+"id": ""
+}
+db_paths = {
+"en": "./data/en_word_emphasis_lookup_mix_homographs.json",
+"sw": "",
+"id": ""
+}
+
+cosmos_config = {
+    "url": os.getenv("COSMOS_DB_URL"),
+    "key": os.getenv("COSMOS_DB_KEY"),
+    "database_name": "Bookbot"
+}
+
+def generate_inputids(text, language):
+    # If not planning to use emphasis then you can set use_cosmos=False and push_oov_to_cosmos=False
+    model = TextProcessor(model_dirs[language], db_paths[language], language=language, use_cosmos=False, cosmos_config=cosmos_config) 
+    return model.get_input_ids(text, phonemes=False, return_phonemes=True, push_oov_to_cosmos=False, add_blank_token=True)
+
+if __name__ == "__main__":
+    print(generate_inputids("Hello <wave> world <listen> how are you? <headLean>", "en"))
+    print(generate_inputids("Halo nama saya Budi. Siapa [nama] kamu?", "id"))
