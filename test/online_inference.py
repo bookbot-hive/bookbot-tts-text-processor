203baa7d1b207f96c43ed12f685a4e3556093fa6
Davidsamuell <davidsamuel.7878@gmail.com>
2025-05-23 23:07:34 +1000

    Update snapshot

--- test/online_inference.py
+++ test/online_inference.py
@@ -24,70 +24,63 @@ def main():
     }
 
     # ENGLISH
-    print("Initializing English model...")
-    model = TextProcessor(
-        model_dirs["en"],
-        db_paths["en"],
-        language="en",
-        use_cosmos=False,
-        cosmos_config=cosmos_config,
-        animation_tags_path="./animation_data.csv",
-        online_g2p=True,
-    )
-    print("Model initialized, performing inference...")
-    result = model.get_input_ids(
-        "Hello",
-        phonemes=False,
-        return_phonemes=True,
-        push_oov_to_cosmos=False,
-        add_blank_token=True,
-    )
+    # print("Initializing English model...")
+    # model = TextProcessor(
+    #     model_dirs["en"],
+    #     db_paths["en"],
+    #     language="en",
+    #     use_cosmos=False,
+    #     cosmos_config=cosmos_config,
+    #     animation_tags_path="./animation_data.csv",
+    #     online_g2p=True,
+    # )
+    # print("Model initialized, performing inference...")
 
-    model = TextProcessor(
-        model_dirs["en"],
-        db_paths["en"],
-        language="en",
-        use_cosmos=False,
-        cosmos_config=cosmos_config,
-        animation_tags_path="./animation_data.csv",
-        online_g2p=True,
-    )
-    result = model.get_input_ids(
-        "Hello World, my name is David! I'm a software engineer, and I love to code.",
-        phonemes=False,
-        return_phonemes=True,
-        push_oov_to_cosmos=True,
-        add_blank_token=True,
-    )
-    print(f"Result: {result}\n")
+    # result = model.get_input_ids(
+    #     "<handRaiseHigh>.",
+    #     phonemes=False,
+    #     return_phonemes=True,
+    #     push_oov_to_cosmos=False,
+    #     add_blank_token=True,
+    # )
+    # print(f"Result: {result}\n")
 
-    result = model.get_input_ids(
-        "Can you [lead] the <sound_part_showing_sound_1> conversation <smile>?",
-        phonemes=False,
-        return_phonemes=True,
-        push_oov_to_cosmos=True,
-        add_blank_token=True,
-    )
-    print(f"Result: {result}")
+    # result = model.get_input_ids(
+    #     "Hello World, my name is David! I'm a software engineer, and I love to code.",
+    #     phonemes=False,
+    #     return_phonemes=True,
+    #     push_oov_to_cosmos=True,
+    #     add_blank_token=True,
+    # )
+    # print(f"Result: {result}\n")
 
-    ### SWAHILI
-    model = TextProcessor(
-        model_dirs["sw"],
-        db_paths["sw"],
-        language="sw",
-        use_cosmos=False,
-        cosmos_config=cosmos_config,
-        animation_tags_path="./animation_data.csv",
-        online_g2p=True,
-    )
-    result = model.get_input_ids(
-        "Jana <handRaiseHigh> nilitembelea mji wa [Nairobi]. 4525 Niliona majengo [marefu] na magari mengi <sound_part_showing_sound_1>.",
-        phonemes=False,
-        return_phonemes=True,
-        push_oov_to_cosmos=False,
-        add_blank_token=True,
-    )
-    print(f"Result: {result}\n")
+    # result = model.get_input_ids(
+    #     "Can you [lead] the <sound_part_showing_sound_1> conversation <smile>?",
+    #     phonemes=False,
+    #     return_phonemes=True,
+    #     push_oov_to_cosmos=True,
+    #     add_blank_token=True,
+    # )
+    # print(f"Result: {result}")
+
+    # ### SWAHILI
+    # model = TextProcessor(
+    #     model_dirs["sw"],
+    #     db_paths["sw"],
+    #     language="sw",
+    #     use_cosmos=False,
+    #     cosmos_config=cosmos_config,
+    #     animation_tags_path="./animation_data.csv",
+    #     online_g2p=True,
+    # )
+    # result = model.get_input_ids(
+    #     "Jana <handRaiseHigh> nilitembelea mji wa [Nairobi]. 4525 Niliona majengo [marefu] na magari mengi <sound_part_showing_sound_1>.",
+    #     phonemes=False,
+    #     return_phonemes=True,
+    #     push_oov_to_cosmos=False,
+    #     add_blank_token=True,
+    # )
+    # print(f"Result: {result}\n")
 
     ### INDONESIAN
     model = TextProcessor(
@@ -99,25 +92,42 @@ def main():
         animation_tags_path="./animation_data.csv",
         online_g2p=True,
     )
-    result = model.get_input_ids(
-        "<sound_scrolling_2> Halo <handRaiseHigh> nama saya Budi siapa [nama] kamu <sound_robotic_arm_2>?",
-        phonemes=False,
-        return_phonemes=True,
-        push_oov_to_cosmos=False,
-        add_blank_token=True,
-    )
-    print(f"Result: {result}\n")
-    result = model.get_input_ids(
-        "Ensiklopedia",
-        phonemes=False,
-        return_phonemes=True,
-        push_oov_to_cosmos=False,
-        add_blank_token=True,
-    )
-    print(f"Result: {result}\n")
+    # result = model.get_input_ids(
+    #     "<sound_scrolling_2> Halo <handRaiseHigh> nama saya Budi siapa [nama] kamu <sound_robotic_arm_2>?",
+    #     phonemes=False,
+    #     return_phonemes=True,
+    #     push_oov_to_cosmos=False,
+    #     add_blank_token=True,
+    # )
+    # print(f"Result: {result}\n")
+    # result = model.get_input_ids(
+    #     "Ensiklopedia",
+    #     phonemes=False,
+    #     return_phonemes=True,
+    #     push_oov_to_cosmos=False,
+    #     add_blank_token=True,
+    # )
+    # print(f"Result: {result}\n")
 
+    # result = model.get_input_ids(
+    #     "Daftar / Masuk. Non-Fiksi",
+    #     phonemes=False,
+    #     return_phonemes=True,
+    #     push_oov_to_cosmos=False,
+    #     add_blank_token=True,
+    # )
+    # print(f"Result: {result}\n")
+    
     result = model.get_input_ids(
-        "Daftar / Masuk. Non-Fiksi",
+        "Minecraft",
+        phonemes=False,
+        return_phonemes=True,
+        push_oov_to_cosmos=False,
+        add_blank_token=True,
+    )
+    print(f"Result: {result}\n")
+    result = model.get_input_ids(
+        "minecraft",
         phonemes=False,
         return_phonemes=True,
         push_oov_to_cosmos=False,
--- test/snapshot_inference.py
+++ test/snapshot_inference.py
@@ -40,10 +40,10 @@ def generate_inputids(text, language):
 
 
 if __name__ == "__main__":
-    print(generate_inputids("a.", "en"))
-    print(generate_inputids("<handRaiseMid>.", "en"))
-    print(generate_inputids(".", "en"))
-    print(generate_inputids("!", "en"))
-    print(generate_inputids("What do you think, Lachlan?", "en"))
-    print(generate_inputids("Halo nama saya Budi. Siapa [nama] kamu?", "id"))
-    print(generate_inputids("Ke.", "id"))
+    # print(generate_inputids("a.", "en"))
+    # print(generate_inputids("<handRaiseMid>.", "en"))
+    # print(generate_inputids(".", "en"))
+    # print(generate_inputids("!", "en"))
+    # print(generate_inputids("What do you think, Lachlan?", "en"))
+    print(generate_inputids("Minecraft", "id"))
+    print(generate_inputids("minecraft", "id"))
